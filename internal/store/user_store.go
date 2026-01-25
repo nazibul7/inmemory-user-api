d27131bec4473936fdc7da4a560fcfeb6d0f7c9b
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2026-01-25 10:29:09 +0530

    add thread-safe in memory UserStore with CURD operations

---
+++ internal/store/user_store.go
@@ -0,0 +1,69 @@
+package store
+
+import (
+	"errors"
+	"sync"
+
+	"github.com/nazibul7/inmemory-user-api/internal/model"
+)
+
+type UserStore struct {
+	mu    sync.Locker
+	users map[string]model.User
+}
+
+func NewUser() *UserStore {
+	return &UserStore{
+		users: make(map[string]model.User),
+	}
+}
+
+func (s *UserStore) CreateUser(user model.User) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.users[user.ID] = user
+}
+
+func (s *UserStore) GetUser(id string) (model.User, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	user, ok := s.users[id]
+	if !ok {
+		return model.User{}, errors.New("User not found")
+	}
+	return user, nil
+}
+
+func (s *UserStore) GetAllUser() []model.User {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	users := make([]model.User, 0, len(s.users))
+	for _, user := range s.users {
+		users = append(users, user)
+	}
+	return users
+}
+
+func (s *UserStore) UpdateUser(id string, user model.User) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if _, ok := s.users[id]; !ok {
+		return errors.New("User not found")
+	}
+	s.users[id] = user
+	return nil
+}
+
+func (s *UserStore) DeleteUser(id string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if _, ok := s.users[id]; !ok {
+		return errors.New("User not found")
+	}
+	delete(s.users, id)
+	return nil
+}
