8c4b429a9e3ef175f50cc9db2257dc711979da89
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2026-01-25 17:34:08 +0530

    connected user store, handlers, and HTTP routes

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/nazibul7/inmemory-user-api/internal/app"
+	"github.com/nazibul7/inmemory-user-api/internal/handler"
+	"github.com/nazibul7/inmemory-user-api/internal/store"
+)
+
 func main() {
-	
+	UserStore := store.NewUserStore()
+	UserHandler := handler.NewUserHandler(UserStore)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", UserHandler.GetAll)
+	mux.HandleFunc("POST /user", UserHandler.Create)
+	mux.HandleFunc("GET /user", UserHandler.GetUser)
+	mux.HandleFunc("PUT /user", UserHandler.UpdateUser)
+	mux.HandleFunc("DELETE /user", UserHandler.DeleteUser)
+
+	server := app.NewServer(":9000", mux)
+
+	log.Println("Server running on :8080")
+	log.Fatal(server.ListenAndServe())
 }
