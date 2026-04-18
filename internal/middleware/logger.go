a31de03bc0743334ab9926c85280db115cd61ebd
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2026-04-18 18:50:17 +0530

    add requestID in logger middleware to track each request properly

--- internal/middleware/logger.go
+++ internal/middleware/logger.go
@@ -9,9 +9,9 @@ import (
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-
+		reqID := r.Context().Value(RequestIDKey).(string)
 		next.ServeHTTP(w, r)
-		
-		log.Printf("%s %s %s", r.Method, r.URL, time.Since(start))
+
+		log.Printf("requestID=%v methode=%s URL=%s duration=%s", reqID, r.Method, r.URL, time.Since(start))
 	})
 }
